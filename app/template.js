65cc38ea824c1f231d47aa4f699809c63ec0d4ce
Giovanni <fiatjaf@gmail.com>
2014-03-25 11:46:18 -0300

    .gitignore compiled js files

--- app/template.js
+++
@@ -1,9 +0,0 @@
-var React, SomeFormOfDisplayOfSomeKindOfThing;
-
-React = require('lib/react');
-
-SomeFormOfDisplayOfSomeKindOfThing = require('components/SomeFormOfDisplayOfSomeKindOfThing');
-
-module.exports = function(browsercode, data) {
-  return "<!DOCTYPE html>\n<html>\n  <head>\n    <title>Curitiba 942</title>\n    <link rel=\"stylesheet\" href=\"/default.css\" type=\"text/css\">\n  </head>\n  <body>\n\n    <div id=\"someKindOfThingList\">\n      " + (React.renderComponentToString(SomeFormOfDisplayOfSomeKindOfThing(data))) + "\n    </div>\n\n  </body>\n\n  <script src=\"//cdnjs.cloudflare.com/ajax/libs/curl/0.7.3/curl/curl.min.js\"></script>\n  <script>\n    window.data = " + (toJSON(data)) + "\n    " + browsercode + "\n  </script>\n\n</html>";
-};
